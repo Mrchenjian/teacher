1638b121814101b0d46a908d26951c8567052de8
独孤九剑 <1845230069@qq.com>
2020-04-21 14:45:15 +0800

    first_upload

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,59 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <div id="myChart" :style="{width: '300px', height: '300px'}"></div>
+    <button @click="$store.dispatch('increment')">重置</button>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import {mapState}from 'vuex'
+export default {
+  name: "hello",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+      hook: this.$store.state.data
+    };
+  },
 
-#nav {
-  padding: 30px;
-}
+  mounted() {
+    this.drawLine();
+  },
+  methods: {
+    drawLine() {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = this.$echarts.init(document.getElementById("myChart"));
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+      // 绘制图表
+      myChart.setOption({
+        title: { text: "在Vue中使用echarts" },
+        tooltip: {},
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: this.hook
+          }
+        ]
+      });
+    }
+  },
+  computed:{
+    ...mapState(['messArray_get'])
+  },
+  watch: {
+    "messArray_get"(val,old) {
+      
+      this.hook = val; // 按照规范在这里应该去使用getters来获取数据
+      
+    }
+  }
+};
+</script>
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style scoped >
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,35 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    data: [5, 20, 36, 7, 4, 50]
+  },
+  getters :{
+    messArray_get:state=>state.data,
   },
   mutations: {
+    rest(state) {
+        var new_arr=state.data
+      for (var i = 0; i < new_arr.length - 1; i++) {
+        for (var j = 0; j < new_arr.length - 1 - i; j++) {
+          if (new_arr[j] > new_arr[j + 1]) {
+            var temp = new_arr[j];
+            new_arr[j] = new_arr[j + 1];
+            new_arr[j + 1] = temp;
+          }
+        }
+      }
+      
+      
+    }
+    
   },
   actions: {
+    increment(state) {
+      state.commit('rest')
+    }
   },
   modules: {
   }
 })
+
+
